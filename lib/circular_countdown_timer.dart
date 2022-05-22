56a03ce7405a4ed1984b8c2f4018337b30a91dca
tnamikawa <takeshi.namikawa@gmail.com>
2022-05-22 21:33:13 +0900

    カウントダウン仕様変更

--- lib/circular_countdown_timer.dart
+++ lib/circular_countdown_timer.dart
@@ -186,7 +186,10 @@ class CircularCountDownTimerState extends State<CircularCountDownTimer>
     } else if (duration.inMinutes != 0) {
       return '${(duration.inMinutes % 60).toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
     } else {
-      return '${duration.inSeconds % 60 + 1}';
+      if (duration.inSeconds == 0) {
+        return 'GO!';
+      }
+      return '${duration.inSeconds % 60}';
     }
   }
 
